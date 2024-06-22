8f3766fbc8960d1be6c550d631e5f972b110ee63
M. Syauqi Fadhlika <ukisyauqi@gmail.com>
2024-06-22 08:47:52 +0700

    init

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import { View, StyleSheet } from "react-native";
+import WeatherSearch from "./components/weatherSearch";
+import WeatherInfo from "./components/weatherInfo";
+import axios from "axios";
+import { API_KEY, BASE_URL } from "./constant";
 
-export default function App() {
+const App = () => {
+  const [weatherData, setWeatherData] = useState()
+  const searchWeather = (location) => {
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data;
+        data.visibility /= 1000
+        data.visibility = data.visibility.toFixed(2)
+        data.main.temp -= 273.15 // Konversi Kelvin ke Celcius
+        data.main.temp = data.main.temp.toFixed(2)
+        setWeatherData(data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Berikan function searchWeather ke component weatherSearch */}
+      <WeatherSearch searchWeather={searchWeather} />
+      {weatherData && <WeatherInfo weatherData={weatherData} />}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
 });
+
+export default App;
---
+++ constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'f0875c4143025757d5818db3ad8b296d';
